db3543495987c221605c4f448334bb12b52f059a
irina <irinamihai50@gmail.com>
2024-12-08 18:10:12 +0000

    add questions

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+VARIABLE_NAME=Sophie
 
+QUESTION1="What's your name?"
+echo $QUESTION1
+read NAME
+echo "Hello $NAME."
 
-echo hello questionnaire
+QUESTION2="Where are you from?"
+
+echo "$QUESTION2"
+read LOCATION
+echo "Hello $NAME" from "$LOCATION"
