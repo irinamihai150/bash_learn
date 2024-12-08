a2cc8bfee9d45749e3861ec3bfac390a348c5c53
irina <irinamihai50@gmail.com>
2024-12-08 18:51:01 +0000

    add final question

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VARIABLE_NAME=Sophie
+echo -e "\n~~Questionnaire~~\n"
 
 QUESTION1="What's your name?"
 echo $QUESTION1
@@ -12,3 +12,11 @@ QUESTION2="Where are you from?"
 echo "$QUESTION2"
 read LOCATION
 echo "Hello $NAME" from "$LOCATION"
+
+QUESTION3="What's your favorite coding website"
+echo "$QUESTION3"
+read WEBSITE
+echo -e "\nHello $NAME  from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
+
+
+
